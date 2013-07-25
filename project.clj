41c96da86b2ab9523029fdd247f083a51ae5b729
Brandon Bloom <brandon@brandonbloom.name>
2013-07-25 15:44:46 -0400

    Make lein happy

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject retree "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A rewrite strategy combinator library, a la StrategoXT"
+  :url "https://github.com/brandonbloom/retree"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
